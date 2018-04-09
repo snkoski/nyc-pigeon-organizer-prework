feb55e0f3a79e0cf14915c4a49980f0cc4309d4e
Shawn Koski <shawnnkoski@gmail.com>
2018-04-09 12:48:53 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -22,7 +22,7 @@ pigeon_data = {
 def nyc_pigeon_organizer(data)
   
   pigeon_list = {}
-	  data.each do |key,values|
+	  data.each do |key,values| #
     values.each do |value,names|
       names.each do |name|
         pigeon_list[name] ||= {}
