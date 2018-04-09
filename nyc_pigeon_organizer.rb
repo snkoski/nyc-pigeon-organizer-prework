b5cbcbdcd3d3a6f11109cf5779e3af2b575acdd3
Shawn Koski <shawnnkoski@gmail.com>
2018-04-09 12:51:46 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -23,7 +23,7 @@ def nyc_pigeon_organizer(data)
   
   pigeon_list = {}
 	  data.each do |key,values| #key == :color, values == {:purple=>["Theo", "Peter Jr.", "Lucky"],:grey=>["Theo", "Peter Jr.", "Ms. K"], etc
-    values.each do |value,names| #value == :purple, names == 
+    values.each do |value,names| #value == :purple, names == ["Theo", "Peter Jr.", "Lucky"]
       names.each do |name|
         pigeon_list[name] ||= {}
         binding.pry
