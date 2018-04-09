f671e2f01ca5e5bf15ed6cee56ec3a5872f3435b
Shawn Koski <shawnnkoski@gmail.com>
2018-04-09 12:39:35 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -22,28 +22,17 @@ pigeon_data = {
 def nyc_pigeon_organizer(data)
   
   pigeon_list = {}
-  data.each do |category, attribute|
-    if category == :color
-    attribute.each do |attribute, name|
-      name.each do |pigeon_name|
-        if pigeon_list.include?(pigeon_name) == false
-          pigeon_list[pigeon_name] = {color: [attribute.to_s]}
-        else
-          pigeon_list[pigeon_name][:color] << attribute.to_s
-          binding.pry
-        end
-      end
-    end
-  end
-  end
-  h = {"Theo" => {:color => ["blue"]}}
-  i = {"Theo" => {:color => ["white"]}}
-  #h.merge(i) { |k, x, y| x + (y ? y : []) }
-  #h.merge(i)
-  #merge_recursively(h, i)
-  #pigeon_list.merge(h)
-  
-  binding.pry
+-end 	+  data.each do |key,values|
++    values.each do |value,names|
++      names.each do |name|
++        pigeon_list[name] ||= {}
++        pigeon_list[name][key] ||= []
++        pigeon_list[name][key] << value.to_s
++      end
++    end
++  end
++  pigeon_list
++end
 end
 #def merge_recursively(a, b)
  # a.merge(b) {|key, a_item, b_item| merge_recursively(a_item, b_item) }
