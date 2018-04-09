9d2d1260cf050026ffa74e55d8f29643c3183bb0
Shawn Koski <shawnnkoski@gmail.com>
2018-04-09 12:54:30 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -30,8 +30,9 @@ def nyc_pigeon_organizer(data)
       names.each do |name| # name == "Theo"
         
         pigeon_list[name] ||= {} # If pigeon_list doesn't contain the key [name] it adds the key with a value of an empty hash
-        binding.pry
+        
         pigeon_list[name][key] ||= []
+        binding.pry
         pigeon_list[name][key] << value.to_s
       end
     end
